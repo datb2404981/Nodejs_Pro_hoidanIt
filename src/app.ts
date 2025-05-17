411728a9e92fbaad6294f19822b250d4386c0964
3 det <datb2404981@student.ctu.edu.vn>
2025-05-17 21:31:35 +0700

    video 33

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 // const express = require("express");
 import express from "express";
+import 'dotenv/config'
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.send("hello world nodemon")
 })
 
 app.get('/chao', (req, res) => {
     res.send("hello van dat")
 })
 
-app.listen(8080, () => {
-    console.log(`My app is running on port : ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`My app is running on port : ${PORT}`);
+    console.log("env port: ", process.env.PORT);
 })
